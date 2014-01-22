2111c139541ceaac7c508e634bc2e21894a8ca8d
sinonep <chiengproject500@outlook.com>
2014-01-21 23:14:07 -0500

    update from steve local pc

--- AProject/src/GitTest.java
+++ AProject/src/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
 		System.out.println("Samip has seen Tuo's change");
 		System.out.println("Stephen was here through web.Still Downloading.");
 		System.out.println("Finally done.");
+		System.out.println("Final testing from local.");
 	}
 	
 	
